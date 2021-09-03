16a35a037a9222a046842b94402aeac7a3be4ed0
Bustatu <bustatu@users.noreply.github.com>
2021-09-03 03:28:00 +0300

    FIFO incorrect output (Nintendo logo is black bars)

--- emulators/gameboy/components/gpu/gpu.cpp
+++ emulators/gameboy/components/gpu/gpu.cpp
@@ -5,6 +5,261 @@
 
 namespace Gameboy
 {
+    uint8_t GPU::get_colour(uint8_t colourNum, uint8_t palette)
+    {
+        switch (colourNum)
+        {
+            case 0:
+                return (get_bit(palette, 1) << 1) | get_bit(palette, 0);
+                break;
+            case 1:
+                return (get_bit(palette, 3) << 1) | get_bit(palette, 2);
+                break;
+            case 2:
+                return (get_bit(palette, 5) << 1) | get_bit(palette, 4);
+                break;
+            case 3:
+                return (get_bit(palette, 7) << 1) | get_bit(palette, 6);
+                break;
+        }
+        return 0;
+    }
+
+    void GPU::tickSpriteFetcher()
+    {
+        // Do stuff only on even ticks
+        if (++spf_ticks % 2 != 0) return;
+
+        // Do stuff depending on the sprite fetcher's state
+        switch(pf_state2)
+        {
+            // Fetch tile no.
+            case 0x00:
+            {
+                sTileNo = cur_sprite.tileIndex;
+                pf_state2 = 0x01;
+                break;
+            }
+            
+            // Fetch data low
+            case 0x01:
+            {
+                uint16_t tileMap = 0x8000;
+                uint16_t offset1 = sTileNo * 16;
+                uint16_t offset2 = 0;
+
+                if(get_bit(cur_sprite.attr, 6))
+                    offset2 = (get_bit(LCDControl, 2) ? 30 : 14) - 2 * (LY - (cur_sprite.y - 16));
+                else
+                    offset2 = 2 * (LY - (cur_sprite.y - 16));
+
+                addr = tileMap + offset1 + offset2;
+                sTileLo = bus -> readByte(addr);
+
+                pf_state2 = 0x02;
+                break;
+            }
+
+            // Fetch data high
+            case 0x02:
+            {
+                sTileHi = bus -> readByte(addr + 1);
+                pf_state2 = 0x03;
+                break;
+            }
+
+            // Push to FIFO
+            case 0x03:
+            {
+                for(int32_t bit = 7, i = 0; bit >= 0; bit--, i++)
+                {
+                    if((cur_sprite.x + i) < 8)
+                        continue;
+
+                    int32_t effectiveBit = get_bit(cur_sprite.attr, 5) ? 7 - bit : bit;
+                    uint8_t colourNum = (get_bit(sTileHi, effectiveBit) << 1) | get_bit(sTileLo, effectiveBit);
+                    Pixel pix{get_colour(colourNum, get_bit(cur_sprite.attr, 4) ? OBP1 : OBP0), colourNum == 0, false, get_bit(cur_sprite.attr, 7) != 0};
+
+                    if(i >= spFIFO.getSize())
+                        spFIFO.push(pix);
+                    else if(spFIFO[i].transparent)
+                        spFIFO[i] = pix;
+                }
+
+                fetchingSprites = false;
+                pf_state1 = 0x00;
+                break;
+            }
+        }
+    }
+
+    void GPU::tickBackgroundFetcher()
+    {
+        // Do stuff only on even ticks
+        if (++bgf_ticks % 2 != 0) return;
+
+        switch (pf_state1)
+        {
+            // Fetch tile no.
+            case 0x00:
+            {
+                uint16_t tileMap;
+                uint16_t xOffset;
+                uint16_t yOffset;
+
+                if (fetchingWindow)
+                {
+                    tileMap = get_bit(LCDControl, 6) ? 0x9C00 : 0x9800;
+                    xOffset = fetcherX;
+                    yOffset = (WLY / 8) * 32;
+                }
+                else
+                {
+                    tileMap = get_bit(LCDControl, 3) ? 0x9C00 : 0x9800;
+                    xOffset = (fetcherX + (SCX / 8)) % 32;
+                    yOffset = (((LY + SCY) % 256) / 8) * 32;
+                }
+
+                addr = tileMap + ((xOffset + yOffset) & 0x3FF);
+                tileNo = bus -> readByte(addr);
+
+                pf_state1 = 0x01;
+                break;
+            }
+
+            // Fetch data low
+            case 0x01:
+            {
+                bool useSigned = !get_bit(LCDControl, 4);
+                uint16_t tileData = useSigned ? 0x9000 : 0x8000;
+                uint16_t offset1 = (useSigned ? int8_t(tileNo) : tileNo) * 16;
+                uint16_t offset2 = fetchingWindow ? (WLY % 8) * 2 : ((LY + SCY) % 8) * 2;
+
+                addr = tileData + offset1 + offset2;
+                tileLo = bus -> readByte(addr);
+
+                pf_state1 = 0x02;
+                break;
+            }
+
+            // Fetch data high
+            case 0x02:
+            {
+                tileHi = bus -> readByte(addr + 1);
+                pf_state1 = 0x03;
+                break;
+            }
+
+            // Push to FIFO
+            case 0x03:
+            {
+                if (bgFIFO.isEmpty())
+                {
+                    for (int bit = 7; bit >= 0; --bit)
+                    {
+                        uint8_t colourNum = (get_bit(tileHi, bit) << 1) | get_bit(tileLo, bit);
+                        bgFIFO.push(Pixel{get_colour(colourNum, BGP), colourNum == 0, get_bit(LCDControl, 0) != 0, false});
+                    }
+
+                    fetcherX++;
+                    pf_state1 = 0x00;
+                }
+                else
+                    pf_state1 = 0x03;
+
+                break;
+            }
+        }
+    }
+
+    Pixel GPU::mixPixels(const Pixel &bg, const Pixel &sp)
+    {
+        if (!sp.transparent)
+        {
+            if (sp.bgPriority && !bg.transparent)
+                return bg;
+            return sp;
+        }
+        return bg;
+    }
+
+    void GPU::tickFetcher()
+    {
+        if (!fetchingSprites && get_bit(LCDControl, 1) && oamBuffer.size() > 0)
+        {
+            Sprite s = oamBuffer.front();
+
+            if ((s.x - 8) <= LX)
+            {
+                cur_sprite = s;
+                pf_state2 = 0x00;
+                spf_ticks = 0;
+                fetchingSprites = true;
+                oamBuffer.pop_front();
+            }
+        }
+
+        if (fetchingSprites)
+        {
+            tickSpriteFetcher();
+            return;
+        }
+
+        if (!fetchingWindow && get_bit(LCDControl, 5) && wyTrigger && LX >= (WX - 7))
+        {
+            fetcherX = 0;
+            pf_state1 = 0x00;
+            fetchingWindow = true;
+            bgFIFO.clear();
+        }
+
+        tickBackgroundFetcher();
+    }
+
+    void PixelFIFO::push(Pixel pix)
+    {
+        buffer[head] = pix;
+        head = (head + 1) % 8;
+        size++;
+    }
+
+    void PixelFIFO::clear()
+    {
+        head = tail = size = 0;
+    }
+
+    bool PixelFIFO::isEmpty()
+    {
+        return size == 0;
+    }
+
+    Pixel PixelFIFO::pop()
+    {
+        Pixel pix = buffer[tail];
+        tail = (tail + 1) % 8;
+        size--;
+        return pix;
+    }
+
+    int32_t PixelFIFO::getSize()
+    {
+        return size;
+    }
+
+    bool GPU::isPFReady()
+    {
+        bool ret = !bgFIFO.isEmpty() && !fetchingSprites;
+
+        if (ret && LX < 0)
+        {
+            bgFIFO.pop();
+            LX++;
+            ret = false;
+        }
+
+        return ret;
+    }
+
     uint8_t GPU::readByte(uint16_t addr)
     {
         return regs[addr - 0xFF40];
@@ -19,8 +274,8 @@ namespace Gameboy
     void GPU::setMode(uint8_t newMode)
     {
         // Sets bits in the stat register
+        set_bit(STAT, 0, get_bit(newMode, 0));
         set_bit(STAT, 1, get_bit(newMode, 1));
-        set_bit(STAT, 2, get_bit(newMode, 2));
 
         // Check if there needs to be an interrupt made
         bool irq = false;
@@ -29,27 +284,44 @@ namespace Gameboy
         {
             // HBlank
             case 0x00:
+            {
                 irq = get_bit(STAT, 3);
                 break;
+            }
 
             // VBlank
             case 0x01:
+            {
                 irq = get_bit(STAT, 4);
 
                 // Request VBlank interrupt
                 bus -> writeByte(0xFF0F, bus -> readByte(0xFF0F) | 1);
                 break;
+            }
+
 
             // OAM search
             case 0x02:
+            {
                 irq = get_bit(STAT, 5);
-                // TODO: clear OAM buffer
+                oamBuffer.clear();
                 break;
+            }
 
             // Pixel transfer (or drawing mode)
             case 0x03:
-                // TODO: init pixel fetcher
+            {
+                // Initialise pixel fetcher
+                fetcherX = 0;
+                LX = -(SCX % 8);
+                pf_state1 = 0x00;
+                bgf_ticks = 0;
+                fetchingSprites = false;
+                fetchingWindow = false;
+                bgFIFO.clear();
+                spFIFO.clear();
                 break;
+            }
         }
 
         // If we got an interrupt
@@ -58,21 +330,31 @@ namespace Gameboy
             bus -> writeByte(0xFF0F, bus -> readByte(0xFF0F) | (1 << 1));
     }
 
-    bool GPU::isOn()
-    {
-        return (LCDControl >> 7) & 1;
-    }
-
     uint8_t GPU::getMode()
     {
-        return STAT & 0b11;
+        return (get_bit(STAT, 1) << 1) | get_bit(STAT, 0);
     }
 
     void GPU::update()
     {
-        // If not on, then don't do anything
-        if(!isOn())
+        // If not on
+        if(!get_bit(LCDControl, 7))
+        {
+            if(!resetFlag)
+            {
+                setMode(0x00);
+                LY = 0;
+                pcnt = 0;
+                memset(pixels, 0, 160 * 144);
+                resetFlag = true;
+            }
             return;
+        }
+        else if(resetFlag)
+        {
+            setMode(0x02);
+            resetFlag = false;
+        }
 
         // Update the clock
         clock++;
@@ -89,7 +371,10 @@ namespace Gameboy
                     pcnt = 0;
                     LY++;
 
-                    // TODO: check if still inside the window
+                    // If we are currently still fetching the window pixels
+                    // also update the window line
+                    if(fetchingWindow)
+                        WLY++;
 
                     // We have entered VBlank
                     if(LY == 144)
@@ -129,12 +414,14 @@ namespace Gameboy
             // OAM search
             case 0x02:
             {
+                // Check if we are on window Y
                 if (!wyTrigger && pcnt == 0)
                     wyTrigger = (LY == WY);
 
                 // Can only do transfers on even clock counts (they take time you know)
                 if(pcnt % 2 == 0)
                 {
+                    // Get sprite adress
                     uint16_t address = 0xFE00 + (pcnt / 2) * 4;
 
                     uint8_t YPos = bus -> readByte(address);
@@ -144,7 +431,7 @@ namespace Gameboy
 
                     uint8_t spriteHeight = get_bit(LCDControl, 2) ? 16 : 8;
 
-                    if(XPos > 0 && (LY + 16) >= YPos && (LY + 16) < (YPos + spriteHeight))
+                    if(XPos > 0 && (LY + 16) >= YPos && (LY + 16) < (YPos + spriteHeight) && oamBuffer.size() < 10)
                     {
                         if(spriteHeight == 16)
                         {
@@ -154,15 +441,16 @@ namespace Gameboy
                                 tileIndex &= 0xFE;
                         }
 
-                        // TODO: push sprite to the OAM buffer
+                        // Push sprite to OAM buffer
+                        oamBuffer.push_back(Sprite{XPos, YPos, tileIndex, attributes, address});
                     }
+                }
 
-                    if (++pcnt == 80)
-                    {
-                        // Sort the buffer and start the pixel transfer
-                        // TODO: sort the buffer
-                        setMode(3);
-                    }
+                if (++pcnt == 80)
+                {
+                    // Sort the buffer and start the pixel transfer
+                    std::sort(oamBuffer.begin(), oamBuffer.end());
+                    setMode(0x03);
                 }
                 break;
             }
@@ -173,10 +461,28 @@ namespace Gameboy
                 // Screen will be updated
                 drawFlag = true;
                 
-                // Update pixel counter
+                // Update pixel counter and fetcher
                 pcnt++;
+                tickFetcher();
+                
+                // If finished processing the pixels
+                if(isPFReady())
+                {
+                    // We can fetch the pixel
+                    Pixel pix = bgFIFO.pop();
 
-                // TODO: do pixel drawing to the screen after fetching from the fetcher
+                    if (!pix.bgEnable)
+                    {
+                        if (!spFIFO.isEmpty())
+                            pix = spFIFO.pop();
+                        else
+                            pix.colour = 0;
+                    }
+                    else if (!spFIFO.isEmpty())
+                        pix = mixPixels(pix, spFIFO.pop());
+
+                    pixels[LX][LY] = pix.colour;
+                }
 
                 // If at the end of the line
                 if(++LX == 160)
@@ -235,5 +541,8 @@ namespace Gameboy
         // Reset everything
         clock = 0;
         cnt = 0;
+
+        SCX = 0;
+        WLY = 0;
     }
 }
--- emulators/gameboy/components/gpu/gpu.hpp
+++ emulators/gameboy/components/gpu/gpu.hpp
@@ -1,6 +1,8 @@
 #ifndef GAMEBOY_GPU_H
 #define GAMEBOY_GPU_H
 
+#include <deque>
+#include <algorithm>
 #include <SDL2/SDL.h>
 #include "../bus/bus.hpp"
 
@@ -12,6 +14,54 @@
 namespace Gameboy
 {
     class Bus;
+
+    struct Pixel
+    {
+        uint8_t colour;
+        bool transparent;
+        bool bgEnable;
+        bool bgPriority;
+    };
+
+    struct Sprite
+    {
+        uint8_t x;
+        uint8_t y;
+        uint8_t tileIndex;
+        uint8_t attr;
+        uint16_t oamAddr;
+
+        bool operator<(const Sprite &other)
+        {
+            if (x == other.x)
+                return oamAddr < other.oamAddr;
+            return x < other.x;
+        }
+    };
+
+    class PixelFIFO
+    {
+    private:
+        Pixel buffer[8];
+        int head, tail, size;
+
+    public:
+        void push(Pixel pix);
+        void clear();
+        bool isEmpty();
+        Pixel pop();
+        int32_t getSize();
+
+        Pixel operator[](int32_t index) const
+        {
+            return buffer[(tail + index) % 8];
+        }
+
+        Pixel &operator[](int32_t index)
+        {
+            return buffer[(tail + index) % 8];
+        }
+    };
     
     class GPU
     {
@@ -26,8 +76,8 @@ namespace Gameboy
         uint8_t regs[0xC] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         uint8_t &LCDControl = regs[0x0];
         uint8_t &STAT = regs[0x1];
-        uint8_t &SCX = regs[0x2];
-        uint8_t &SCY = regs[0x3];
+        uint8_t &SCY = regs[0x2];
+        uint8_t &SCX = regs[0x3];
         uint8_t &LY = regs[0x4];
         uint8_t &LYC = regs[0x5];
         uint8_t &DMA = regs[0x6];
@@ -38,19 +88,53 @@ namespace Gameboy
         uint8_t &WX = regs[0xB];
 
         // LY but for the X axis
-        uint8_t LX;
+        uint8_t LX = 0;
+
+        // OAM buffer
+        std::deque<Sprite> oamBuffer;
 
         // Window line counter
-        uint32_t WLY;
+        uint32_t WLY = 0;
 
         // True when LY = WY
-        bool wyTrigger;
+        bool wyTrigger = false;
 
         // Frame counter
         uint32_t cnt;
 
         // Pixel counter
-        uint32_t pcnt;
+        uint32_t pcnt = 0;
+
+        // PIXEL FETCHER REGISTERS
+        int fetcherX;               // can be understood as an X-position in an 32x32 tile map
+        int pf_state1;              // state of the pixel fetcher (for fetching background or window pixels)
+        int pf_state2;              // state of the pixel fetcher (for fetching sprite pixels)
+        int bgf_ticks;              // for making sure that the background pixel fetcher runs every 2 cycles
+        int spf_ticks;              // for making sure that the sprite pixel fetcher runs every 2 cycles
+        bool fetchingSprites;       // true if currently fetching sprite pixels, false otherwise
+        bool fetchingWindow;        // true if currently fetching window pixels, false otherwise
+        Sprite cur_sprite;          // temporary variable for use in PPU::TickSpritePixFetcher
+        uint16_t addr;              // temporary variable for use in PPU::TickFetcher
+        uint8_t tileNo;             // tile number (set in PF_GET_TILE_NO mode)
+        uint8_t tileLo;             // the lower byte of a tile (set in PF_GET_TDATA_LO)
+        uint8_t tileHi;             // the upper byte of a tile (set in PF_GET_TDATA_HI)
+        uint8_t sTileNo;            // tile number (set in PF_GET_TILE_NO mode when fetching sprite pixels)
+        uint8_t sTileLo;            // the lower byte of a tile (set in PF_GET_TDATA_LO when fetching sprite pixels)
+        uint8_t sTileHi;            // the upper byte of a tile (set in PF_GET_TDATA_HI when fetching sprite pixels)
+
+        PixelFIFO bgFIFO;           // background FIFO
+        PixelFIFO spFIFO;           // sprite FIFO
+
+        // Update the fetchers
+        void tickFetcher();
+        void tickBackgroundFetcher();
+        void tickSpriteFetcher();
+
+        // Gets a color from a palette
+        uint8_t get_colour(uint8_t colourNum, uint8_t palette);
+
+        // Mix 2 pixels
+        Pixel mixPixels(const Pixel &bg, const Pixel &sp);
 
         // Changes display mode to a new value
         void setMode(uint8_t newMode);
@@ -61,15 +145,21 @@ namespace Gameboy
         // Draw flag controls if the screen should be updated
         bool drawFlag = true;
 
+        // Reset flag
+        bool resetFlag = false;
+
         // Gets current display mode
         uint8_t getMode();
+
+        // Gets if the GPU is currently on
+        bool isOn();
+
+        // Finds out if pixel fetcher is ready
+        bool isPFReady();
 
     public:
         // Constructor
         GPU();
-
-        // Gets if the GPU is currently on
-        bool isOn();
 
         // Update
         void update();
